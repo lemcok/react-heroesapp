3499b1ad4dae17f8c6f2770e6a0c8aedc7df7409
Ricardo Flores <rik.fm1809@gmail.com>
2020-11-19 18:32:26 -0500

    upload heros-app

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import { useContext } from 'react'
+import { AuthContext } from '../../auth/AuthContext'
+import { types } from '../../types/types';
 
 export const LoginScreen = ({ history }) => {
 
+    const { dispatch } = useContext( AuthContext );
+
     const handleLogin = () => {
         // history.push('/')
-        history.replace('/')
+        // history.replace('/')
+
+        const lastPath = localStorage.getItem('lastPath') || '/';
+
+        dispatch({
+            type: types.login,
+            payload: {
+                name: 'Ricardo'
+            }
+        });
+
+        history.replace( lastPath );
     }
     
 
@@ -21,4 +37,4 @@ export const LoginScreen = ({ history }) => {
             </button>
         </div>
     )
-}
+}   
